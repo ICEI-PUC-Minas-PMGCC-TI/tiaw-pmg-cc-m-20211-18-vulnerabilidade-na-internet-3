10047c4c3f81dd25d27b5df43980c9034d671da4
LOB79 <84478376+LOB79@users.noreply.github.com>
2021-06-03 16:21:27 -0300

    Update and rename app.js to appdados.js

---
+++ assets/js/appdados.js
@@ -0,0 +1,57 @@
+function lelogin (){
+    let strlogin = localStorage.getItem('db');
+    let objsite = {};
+
+    if (strlogin) {
+        objsite = JSON.parse (strlogin);
+    }
+    else {
+        objsite = { 
+            logins: [
+                         {site:"Google", user:"Leo", senha:"leo42"},
+                         {site:"Amazon", user:"Leonardo", senha:"leo2"}
+            ]
+        }
+
+        }
+        return objsite; 
+}
+
+function salvalogin (login) {
+    localStorage.setItem ('db' , JSON.stringify (login));
+}
+function imprimirlogin (){
+    let tela = document.getElementById('tela');
+    let tela1 = document.getElementById('tela1');
+    let tela2 = document.getElementById('tela2');
+    let strHtml1 = '';
+    let strHtml2 = '';
+    let strHtml3 = '';
+    let objsite = lelogin ();
+
+    for (i=0; i < objsite.logins.length; i++){
+        strHtml1 = strHtml1 +  `<p> ${objsite.logins[i].site} </p>`
+        strHtml2 = strHtml2 +  `<p> ${objsite.logins[i].user} </p>`
+        strHtml3 = strHtml3 +  `<p> ${objsite.logins[i].senha} </p>`
+    }
+    tela.innerHTML = strHtml1;
+    tela1.innerHTML = strHtml2;
+    tela2.innerHTML = strHtml3;
+}
+function incluirlogin (){
+    let objsite = lelogin();
+    let nsite = document.getElementById("bdsite").value;
+    let nuser = document.getElementById("bduser").value;
+    let nsenha = document.getElementById("bdsenha").value;
+    let novasenha = {
+        site: nsite,
+        user: nuser,
+        senha: nsenha
+    };
+    objsite.logins.push (novasenha);
+    salvalogin (objsite);
+
+ imprimirlogin();   
+}
+document.getElementById ('bdbsalvar').addEventListener ('click', incluirlogin);
+document.getElementById ('bdbutton').addEventListener ('click', imprimirlogin);
