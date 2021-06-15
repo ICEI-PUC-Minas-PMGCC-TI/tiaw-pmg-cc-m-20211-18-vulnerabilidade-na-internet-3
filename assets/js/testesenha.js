aa7353cdb204e3a6fa03638a5151cc32e5dc4113
LOB79 <84478376+LOB79@users.noreply.github.com>
2021-06-15 18:44:46 -0300

    Add files via upload

---
+++ assets/js/testesenha.js
@@ -0,0 +1,25 @@
+function testesenha() {
+    var senha = document.getElementById('testsenha');
+
+    var Num = /[0-9]/;
+    var LeMaus = /[A-Z]/;
+    var LeMin = /[a-z]/;
+    var CaracEsp = /[Ç|ç|!|@|#|$|%|^|&|*|(|)|-|_]/;
+
+    if (senha.value.length == 0) {
+        alert('Coloque a senha que ira testar\nDica:\nNao use nomes conhecidos e datas');
+    } else if (senha.value.length < 12) {
+        alert('Senha Fraca!!!! \nDica:\nUma boa senha tem no minimo 12 caracteres');
+    } else if (!senha.value.match(Num)) {
+        alert('Senha Fraca!!!! \nDica:\nAdicione numerais');
+    } else if (!senha.value.match(LeMaus)) {
+        alert('Senha Fraca!!!! \nDica:\nAdicione letra maiuscula');
+    } else if (!senha.value.match(LeMin)) {
+        alert('Senha Mediana!!!! \nDica:\nAdicione letra minuscula');
+    }else if (!senha.value.match(CaracEsp)) {
+        alert('Senha Mediana!!!! \nDica:\nAdicione ao menos um caractere especial');
+    } else {
+        alert('Esta e uma senha Forte');
+    }
+}
+document.getElementById ('btttest').addEventListener ('click', testesenha);
